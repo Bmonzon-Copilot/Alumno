abd54fc32c48c554bac06c2022f6bb58a6900ec5
Bmonzon <bmonzon1024@gmail.com>
2025-07-11 21:24:25 -0600

    commit 3

--- Alumno.py
+++ Alumno.py
@@ -1,15 +1,29 @@
 class Estudiante:  #Clase Cliente
-    def __init__(self, nombre, carnet, carrera, nota_final): #Costructor
+    def __init__(self, nombre, carnet, carrera, notafinal): #Costructor
         self.__nombre = nombre
         self.__carnet = carnet
         self.__carrera = carrera
-        self.__nota_final = nota_final
+        self.__notaFinal = notafinal
+
+class notas:
+    def __init__(self, nota1, nota2, nota3):
+        self.__nota1 = nota1
+        self.__nota2 = nota2
+        self.__nota3 = nota3
+
 
  def registrar_estudiante(self):
         nombre = input("Ingrese Nombre: ")
         carnet = input("Ingrese carnet: ")
         carrera = input("Ingrese carrera: ")
         nota_ = input("Correo: ")
+
+ def menu(self):
+            while True:
+                print("\n--- estudiante ---")
+                print("1. Ingrese nombre")
+                print("2. Ingrese de notas")
+                print("3. Ver Notas")
 
 
 
